a3f9a5dbbd905ad22b1e8f6b2446e175657a07dc
rishabdudhia <rishabdudhia@gmail.com>
2022-05-28 00:35:37 -0700

    parse and normalize data

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from tree import Tree
+from validator import Validator
+print ("Welcome to Rishab Dudhia's Feature Search Algorithm")
+print ("Type in the name of the file to test: ")
+file = input()
 
-print ("Welcome to Rishab Dudhia's Feature Search Algorithm\n")
-print ("Please enter the total number of features: ")
-numFeatures = input()
-print("\nType the number of the algorithm you wish to run:")
-print("1. Forward Selection")
-print("2. Backwards Elimination")
-alg = int(input())
+# print("\n")
+# print ("Please enter the total number of features: ")
+# numFeatures = input()
+# print("\nType the number of the algorithm you wish to run:")
+# print("1. Forward Selection")
+# print("2. Backwards Elimination")
+# alg = int(input())
 
-features = []
-for i in range(int(numFeatures)):
-    features.append(i + 1)
-tree = Tree(features)
+    
+val = Validator(file)
+# features = []
+# for i in range(int(numFeatures)):
+#     features.append(i + 1)
+# tree = Tree(features)
 # tree.printTree()
-tree.solve(alg)
+# tree.solve(alg)
